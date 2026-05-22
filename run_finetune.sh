9bafa195dcae58abe3120a1a93cbd502323069b3
anajade <anajadeboudreault@gmail.com>
2026-05-22 13:12:14 +0200

    adjust scripts for hpc

--- run_finetune.sh
+++ run_finetune.sh
@@ -12,7 +12,7 @@ conda activate pytorch-env
 HOME2="/home/Boudreault/Dokumente"
 for approach in simclr byol
 do
-  for arch in resnet18 mobilenetv3 pvtv2b0
+  for arch in resnet18 # mobilenetv3 pvtv2b0
   do
     # Run finetune from 5 to 20% of the lab data
   python $HOME2/OCT_classification/finetune_model.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.05 --dataset_name oct --approach $approach --arch $arch
@@ -20,6 +20,6 @@ do
   python $HOME2/OCT_classification/finetune_model.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.15 --dataset_name oct --approach $approach --arch $arch
   python $HOME2/OCT_classification/finetune_model.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.20 --dataset_name oct --approach $approach --arch $arch
   # Run finetune on 100% of the clinical data
-  python $HOME2/OCT_classification/finetune_model.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --approach $approach --arch $arch
+  # python $HOME2/OCT_classification/finetune_model.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --approach $approach --arch $arch
   done
 done
--- run_supervised_train.sh
+++ run_supervised_train.sh
@@ -10,7 +10,7 @@ conda activate pytorch-env
 # echo $(pwd)
 
 HOME2="/home/Boudreault/Dokumente"
-for arch in resnet18 mobilenetv3 pvtv2b0
+for arch in resnet18 # mobilenetv3 pvtv2b0
   do
   # Run train from 5 to 20% of the lab data starting from random weights
   python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.05 --dataset_name oct --weight_init random --arch $arch
@@ -23,8 +23,8 @@ for arch in resnet18 mobilenetv3 pvtv2b0
   python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.15 --dataset_name oct --weight_init default --arch $arch
   python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 0.20 --dataset_name oct --weight_init default --arch $arch
   # Run train on 100% of the clinical data starting random weights
-  python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --weight_init random --arch $arch
+  # python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --weight_init random --arch $arch
   # Run train on 100% of the clinical data starting imagenet weights
-  python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --weight_init default --arch $arch
+  # python $HOME2/OCT_classification/train_supervised.py --config $HOME2/OCT_classification/config.yaml --ratio_sup 1 --dataset_name oct_clinical --weight_init default --arch $arch
 done
 #
